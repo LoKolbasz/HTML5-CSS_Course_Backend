46fb9dbb95b0687f283e8fb137e2f7b8079a3844
Dániel Battyányi <86350313+LoKolbasz@users.noreply.github.com>
2022-12-07 23:17:03 +0100

    Finalization on Models

--- HTML5-CSS_Course_Backend_Models/Reservation.cs
+++ HTML5-CSS_Course_Backend_Models/Reservation.cs
@@ -1,27 +1,41 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace HTML5_CSS_Course_Backend_Models
 {
+    [JsonSerializable(typeof(Reservation))]
     public class Reservation
     {
+        #nullable disable
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public string? id { get; set; }
         [MaxLength(20)]
         [MinLength(3)]
-        public string? name { get; set; }    //Foglaló neve
-        public string? contact { get; set; } //Foglalás kezdete (pl.: 2021-10-10T10:00:10)
+        [Required]
+        public string name { get; set; }    //Foglaló neve
+        [Required]
+        public string contact { get; set; } //Foglalás kezdete (pl.: 2021-10-10T10:00:10)
         public DateTime begin { get; set; } //Foglalás kezdete
         public DateTime end { get; set; }   //Foglalás vége
         [NotMapped]
         public virtual Table ? table { get; set; }
         [ForeignKey(nameof(Table))]
+        [Range(1,long.MaxValue)]
         [JsonIgnore]
         public long tableId { get; set; }
-        public byte person { get; set; }    //Személyek száma
+        [NotNull]
+        public byte[] person { get; set; }    //Személyek száma
 
-        public Reservation(string id, string name, string contact, DateTime begin, DateTime end, long tableId, byte person)
+        public static string dateTimeFormat = "yyyy-MM-ddThh:mm:ss";
+        public static IFormatProvider formatProvider = new CultureInfo("hu-HU");
+        public static DateTimeStyles dateTimeStyles = DateTimeStyles.AllowInnerWhite;
+
+        public Reservation(string id, string name, string contact, DateTime begin, DateTime end, long tableId, string person)
         {
             this.id = id;
             this.name = name;
@@ -29,11 +43,30 @@ namespace HTML5_CSS_Course_Backend_Models
             this.begin = begin;
             this.end = end;
             this.tableId = tableId;
-            this.person = person;
+            this.person = Encoding.UTF8.GetBytes(person);
         }
 
         public Reservation()
         {
         }
+        public bool CompareDates(DateTime start, DateTime stop) => start <= this.begin || start <= this.end || stop >= this.begin || stop >= this.end;
+
+        public override bool Equals(object obj)
+        {
+            return obj is Reservation reservation &&
+                   name == reservation.name &&
+                   contact == reservation.contact &&
+                   begin == reservation.begin &&
+                   end == reservation.end &&
+                   tableId == reservation.tableId &&
+                   EqualityComparer<byte[]>.Default.Equals(person, reservation.person);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(name, contact, begin, end, tableId, person);
+        }
+
+#nullable restore
     }
 }
--- HTML5-CSS_Course_Backend_Models/Table.cs
+++ HTML5-CSS_Course_Backend_Models/Table.cs
@@ -1,30 +1,48 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace HTML5_CSS_Course_Backend_Models;
+[JsonSerializable(typeof(Table))]
 public class Table
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Key]
     public long id { get; set; }
     [Required]
     [MaxLength(20)]
     [MinLength(0)]
     public string ? name { get; set; } //Asztal elnevezése
-    [Required]
-    public byte capacity { get; set; } //székek száma az asztalnál
+    public byte[] capacity { get; set; } //székek száma az asztalnál
     [NotMapped]
     [JsonIgnore]
-    public ICollection<Reservation> ? reservations { get; set; }
+    public virtual ICollection<Reservation> ? reservations { get; set; }
 
-    public Table(long id, string name, byte capacity)
+    [JsonIgnore]
+    public bool isEnabled { get; set; }
+
+    public Table(long id, string name, string capacity)
     {
         this.id = id;
         this.name = name;
-        this.capacity = capacity;
+        this.capacity = Encoding.UTF8.GetBytes(capacity);
+        this.isEnabled = false;
     }
 
     public Table()
     {
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Table table &&
+               name == table.name &&
+               EqualityComparer<byte[]>.Default.Equals(capacity, table.capacity);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(name, capacity);
     }
 }
